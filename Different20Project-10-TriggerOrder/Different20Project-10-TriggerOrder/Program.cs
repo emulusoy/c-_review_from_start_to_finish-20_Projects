b179008dd0db58ebec91cd27b73fa65cc74c6542
Mustafa Ulusoy <116921446+emulusoy@users.noreply.github.com>
2025-01-29 19:52:01 +0300

    16

---
+++ Different20Project-10-TriggerOrder/Different20Project-10-TriggerOrder/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Different20Project_10_TriggerOrder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Entities1 context = new Entities1();
+
+            Console.WriteLine("##############");
+
+            Console.WriteLine("1-urun listesi");
+            Console.WriteLine("2-siparis listesi");
+            Console.WriteLine("3-Kasa durumu");
+            Console.WriteLine("4-yeni urun satisi");
+            Console.WriteLine("5-urun stok guncelle");
+            Console.WriteLine();
+            Console.WriteLine("##############");
+
+            Console.Write("SECCC");
+            string number= Console.ReadLine();
+
+            if (number == "1")
+            {
+                var values=context.TblProduct.ToList(); 
+                foreach (var item in values)
+                {
+                    Console.WriteLine(item.ProductId + item.ProductName+item.ProductPrice+item.ProductStock);
+                }
+            }
+        }
+    }
+}
