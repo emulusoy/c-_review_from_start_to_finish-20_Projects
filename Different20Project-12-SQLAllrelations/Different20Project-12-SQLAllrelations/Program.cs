d4e2ee95507800d4aefdbbebb0ab32963015ceec
Mustafa Ulusoy <116921446+emulusoy@users.noreply.github.com>
2025-01-29 12:00:09 +0300

    12

---
+++ Different20Project-12-SQLAllrelations/Different20Project-12-SQLAllrelations/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Different20Project_12_SQLAllrelations
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
