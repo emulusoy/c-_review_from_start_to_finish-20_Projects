23a1b432aa42e875ce44e55b1a9c364cd44a9995
Mustafa Ulusoy <116921446+emulusoy@users.noreply.github.com>
2025-01-29 11:19:15 +0300

    11

---
+++ Different20Project-11-WeatherApi/Different20Project-11-WeatherApi/Form1.Designer.cs
@@ -0,0 +1,221 @@
+﻿namespace Different20Project_11_WeatherApi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblc = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.lbl123 = new System.Windows.Forms.Label();
+            this.lblWind = new System.Windows.Forms.Label();
+            this.lblhumidity = new System.Windows.Forms.Label();
+            this.lblfah = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(21, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(164, 33);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ISTANBUL";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(22, 70);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(146, 29);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Istanbul TR";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(22, 117);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(87, 29);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Bugun";
+            // 
+            // lblc
+            // 
+            this.lblc.AutoSize = true;
+            this.lblc.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblc.ForeColor = System.Drawing.Color.White;
+            this.lblc.Location = new System.Drawing.Point(334, 254);
+            this.lblc.Name = "lblc";
+            this.lblc.Size = new System.Drawing.Size(117, 42);
+            this.lblc.TabIndex = 3;
+            this.lblc.Text = "00.00";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.ForeColor = System.Drawing.Color.White;
+            this.label5.Location = new System.Drawing.Point(433, 225);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(31, 29);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "C";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label6.ForeColor = System.Drawing.Color.White;
+            this.label6.Location = new System.Drawing.Point(337, 296);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(61, 20);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Bugun";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label7.ForeColor = System.Drawing.Color.White;
+            this.label7.Location = new System.Drawing.Point(55, 422);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(95, 29);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Ruzgar";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label8.ForeColor = System.Drawing.Color.White;
+            this.label8.Location = new System.Drawing.Point(82, 476);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(68, 29);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "Nem";
+            // 
+            // lbl123
+            // 
+            this.lbl123.AutoSize = true;
+            this.lbl123.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lbl123.ForeColor = System.Drawing.Color.White;
+            this.lbl123.Location = new System.Drawing.Point(12, 526);
+            this.lbl123.Name = "lbl123";
+            this.lbl123.Size = new System.Drawing.Size(138, 29);
+            this.lbl123.TabIndex = 8;
+            this.lbl123.Text = "Fahrenheit";
+            // 
+            // lblWind
+            // 
+            this.lblWind.AutoSize = true;
+            this.lblWind.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblWind.ForeColor = System.Drawing.Color.White;
+            this.lblWind.Location = new System.Drawing.Point(184, 429);
+            this.lblWind.Name = "lblWind";
+            this.lblWind.Size = new System.Drawing.Size(54, 20);
+            this.lblWind.TabIndex = 9;
+            this.lblWind.Text = "00.00";
+            // 
+            // lblhumidity
+            // 
+            this.lblhumidity.AutoSize = true;
+            this.lblhumidity.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblhumidity.ForeColor = System.Drawing.Color.White;
+            this.lblhumidity.Location = new System.Drawing.Point(184, 483);
+            this.lblhumidity.Name = "lblhumidity";
+            this.lblhumidity.Size = new System.Drawing.Size(54, 20);
+            this.lblhumidity.TabIndex = 10;
+            this.lblhumidity.Text = "00.00";
+            // 
+            // lblfah
+            // 
+            this.lblfah.AutoSize = true;
+            this.lblfah.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblfah.ForeColor = System.Drawing.Color.White;
+            this.lblfah.Location = new System.Drawing.Point(184, 533);
+            this.lblfah.Name = "lblfah";
+            this.lblfah.Size = new System.Drawing.Size(54, 20);
+            this.lblfah.TabIndex = 11;
+            this.lblfah.Text = "00.00";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(6)))), ((int)(((byte)(87)))));
+            this.ClientSize = new System.Drawing.Size(573, 605);
+            this.Controls.Add(this.lblfah);
+            this.Controls.Add(this.lblhumidity);
+            this.Controls.Add(this.lblWind);
+            this.Controls.Add(this.lbl123);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.lblc);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.ForeColor = System.Drawing.Color.Black;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblc;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label lbl123;
+        private System.Windows.Forms.Label lblWind;
+        private System.Windows.Forms.Label lblhumidity;
+        private System.Windows.Forms.Label lblfah;
+    }
+}
+
---
+++ Different20Project-11-WeatherApi/Different20Project-11-WeatherApi/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json.Linq;
+
+namespace Different20Project_11_WeatherApi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        double c;
+        private async void Form1_Load(object sender, EventArgs e)
+        {
+            var client = new HttpClient();
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri("https://open-weather13.p.rapidapi.com/city/Istanbul/EN"),
+                Headers =
+    {
+        { "x-rapidapi-key", "9cd394884emsh146f4c4615c50c7p1acc61jsn8ee26bba35b9" },
+        { "x-rapidapi-host", "open-weather13.p.rapidapi.com" },
+    },
+            };
+            using (var response = await client.SendAsync(request))
+            {
+                response.EnsureSuccessStatusCode();
+                var body = await response.Content.ReadAsStringAsync();
+
+                //yukarisi hazir api kodalari
+                var json = JObject.Parse(body);
+                var valuefah = json["main"]["feels_like"].ToString();//bu apideki deger! once maine git sonra digerine
+                lblfah.Text = valuefah.ToString();
+
+                var wind = json["wind"]["speed"].ToString();
+                lblWind.Text = wind.ToString();
+                c = (double.Parse(valuefah) - 32) / 1.8;
+                lblc.Text = c.ToString("00.0");
+
+                var humidity = json["main"]["humidity"].ToString();
+                lblhumidity.Text = humidity.ToString();
+                ;
+            }
+        }
+    }
+}
